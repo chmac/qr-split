f2752bb0b1b8fd751c89032548155225d7a633b0
Callum Macdonald <git@callum-macdonald.com>
2025-11-16 20:23:21 +0100

    Split into arbitrary pieces. Basic docs.

--- main.ts
+++ main.ts
@@ -1,47 +1,63 @@
 import encodeQR, { Bitmap } from "@paulmillr/qr";
 
-function splitBooleanGrid(
-  grid: boolean[][]
-): [boolean[][], boolean[][], boolean[][]] {
-  return grid.reduce<[boolean[][], boolean[][], boolean[][]]>(
-    (acc, row) => {
-      const [a, b, c] = acc;
+const usage = `deno --allow-write=output run main.ts 3 "Some string to encode"`;
 
-      const [nrA, nrB, nrC] = row.reduce<[boolean[], boolean[], boolean[]]>(
-        ([ra, rb, rc], val) => {
-          if (!val) {
-            return [
-              [...ra, false],
-              [...rb, false],
-              [...rc, false],
-            ];
-          }
-
-          const which = Math.floor(Math.random() * 3);
-
-          return [
-            [...ra, which === 0],
-            [...rb, which === 1],
-            [...rc, which === 2],
-          ];
-        },
-        [[], [], []]
-      );
-
-      return [
-        [...a, nrA],
-        [...b, nrB],
-        [...c, nrC],
-      ];
-    },
-    [[], [], []]
+function splitBooleanGridN(grid: boolean[][], pieces: number): boolean[][][] {
+  const initial: boolean[][][] = Array.from(
+    { length: pieces },
+    () => [] as boolean[][]
   );
+
+  return grid.reduce<boolean[][][]>((acc, row) => {
+    const newRows: boolean[][] = Array.from(
+      { length: pieces },
+      () => [] as boolean[]
+    );
+
+    row.reduce<boolean[][]>((rowsAcc, val) => {
+      if (!val) {
+        for (let i = 0; i < pieces; i++) {
+          rowsAcc[i].push(false);
+        }
+        return rowsAcc;
+      }
+
+      const which = Math.floor(Math.random() * pieces);
+      for (let i = 0; i < pieces; i++) {
+        rowsAcc[i].push(i === which);
+      }
+      return rowsAcc;
+    }, newRows);
+
+    return acc.map((g, i) => [...g, newRows[i]]);
+  }, initial);
 }
 
-const text = "hello-foo-bar";
+const [outputPieceCountArgument, text] = Deno.args;
+
+if (typeof outputPieceCountArgument === "undefined") {
+  console.error("ERROR: Number of pieces not specified");
+  console.log(usage);
+  Deno.exit();
+}
+
+const outputPieceCount = parseInt(outputPieceCountArgument);
+
+if (!(outputPieceCount > 0)) {
+  console.error("ERROR: Invalid number of pieces");
+  console.log(usage);
+  Deno.exit();
+}
+
+if (typeof text !== "string" || text.length === 0) {
+  console.error("ERROR: Invalid input string provided");
+  console.log(usage);
+  Deno.exit();
+}
+
 const array = encodeQR(text, "raw");
 
-const arrays = splitBooleanGrid(array);
+const arrays = splitBooleanGridN(array, 3);
 
 const transparentArrays = arrays.map((array) =>
   array.map((row) => row.map((cell) => (cell ? true : undefined)))
@@ -55,5 +71,5 @@ transparentArrays.forEach((array, outputIndex) => {
 
   const output = bitmap.toSVG();
 
-  Deno.writeTextFileSync(`./output-${outputIndex}.svg`, output);
+  Deno.writeTextFileSync(`./output/output-${outputIndex}.svg`, output);
 });
